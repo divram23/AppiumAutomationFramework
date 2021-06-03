0337d05ce8659e001ccd4fd73c228e8b270be4c8
unknown <divya.2387@gmail.com>
2021-06-03 12:10:08 +0530

    second commit

---
+++ src/main/java/resources/startEmulator.bat
@@ -0,0 +1,2 @@
+cd C:\Users\pc\AppData\Local\Android\Sdk\emulator
+emulator -avd DivyaTest
--- src/test/java/GeneralStoreTesting/BaseTest.java
+++ src/test/java/GeneralStoreTesting/BaseTest.java
@@ -3,22 +3,66 @@ package GeneralStoreTesting;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.ServerSocket;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    //public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException {
+   
+         public static AndroidDriver<AndroidElement>  driver;
+         public static AppiumDriverLocalService service;
+	  
+	
+	public AppiumDriverLocalService startServer()
+	{
+		//
+	boolean flag=	checkIfServerIsRunnning(4723);
+	if(!flag)
+	{
+		
+		service= AppiumDriverLocalService.buildDefaultService();
+		service.start();
+	}
+		return service;
+		
+	}
+	
+public static boolean checkIfServerIsRunnning(int port) {
+		
+		boolean isServerRunning = false;
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(port);
+			
+			serverSocket.close();
+		} catch (IOException e) {
+			//If control comes here, then it means that the port is in use
+			isServerRunning = true;
+		} finally {
+			serverSocket = null;
+		}
+		return isServerRunning;
+	}
+public static void startEmulator() throws IOException, InterruptedException
+{
 
-    public static AndroidDriver<AndroidElement>  driver;
+	Runtime.getRuntime().exec(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\startEmulator.bat");
+	Thread.sleep(6000);
+}
 
     @BeforeMethod
-    public void setup() throws MalformedURLException {
+    public void setup() throws IOException, InterruptedException {
+
+		    startServer();
+		    startEmulator();
 
             File appDir = new File("src");
             File app = new File(appDir, "General-Store.apk");
